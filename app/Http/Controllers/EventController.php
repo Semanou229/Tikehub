de520917e1bfa02edc91c21e6835f5b0bb285bfd
Semanou229 <semanou229@github.com>
2025-11-29 16:06:46 +0100

    fix: Ajout de l'import GeocodingService dans EventController

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Event;
+use App\Services\GeocodingService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
 use Illuminate\Support\Facades\Storage;
