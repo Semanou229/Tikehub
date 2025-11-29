4ae9e035a1f03714a826e8874b230026a148493d
Semanou229 <semanou229@github.com>
2025-11-29 14:58:17 +0100

    fix: Ajout de la colonne kyc_submitted_at manquante dans la table users

--- app/Models/User.php
+++ app/Models/User.php
@@ -39,6 +39,7 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
         'kyc_verified_at' => 'datetime',
+        'kyc_submitted_at' => 'datetime',
         'password' => 'hashed',
         'is_active' => 'boolean',
     ];
