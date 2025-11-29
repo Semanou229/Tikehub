621486dfc23ddf1b12da16fd1a88af97f642dc4a
Semanou229 <semanou229@github.com>
2025-11-29 14:11:52 +0100

    fix: Ajout du paramètre Request manquant dans TeamTaskController@index

--- app/Http/Controllers/Organizer/Crm/TeamTaskController.php
+++ app/Http/Controllers/Organizer/Crm/TeamTaskController.php
@@ -10,7 +10,7 @@ use Illuminate\Http\Request;
 
 class TeamTaskController extends Controller
 {
-    public function index(Team $team)
+    public function index(Request $request, Team $team)
     {
         $this->authorize('view', $team);
 
